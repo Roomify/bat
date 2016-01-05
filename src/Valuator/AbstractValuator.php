ecf4797936b62a9cdb6d4f5ef12855b959cc3a93
istos <ronald@roomify.us>
2016-01-05 12:57:21 +0100

    Add basic getters and setters for AbstractValuator

--- src/Valuator/AbstractValuator.php
+++ src/Valuator/AbstractValuator.php
@@ -8,6 +8,7 @@
 namespace Roomify\Bat\Valuator;
 
 use Roomify\Bat\Valuator\ValuatorInterface;
+use Roomify\Bat\Unit\Unit;
 
 abstract class AbstractValuator implements ValuatorInterface {
 
@@ -31,4 +32,66 @@ abstract class AbstractValuator implements ValuatorInterface {
    * @var
    */
   protected $unit;
+
+  /**
+   * AbstractValuator constructor.
+   * @param \DateTime $start_date
+   * @param \DateTime $end_date
+   * @param \Roomify\Bat\Unit\Unit $unit
+   */
+  public function __construct(\DateTime $start_date, \DateTime $end_date, Unit $unit) {
+    $this->start_date = clone($start_date);
+    $this->end_date = clone($end_date);
+    $this->unit = $unit;
+  }
+
+  /**
+   * @param \DateTime $start_date
+   */
+  public function setStartDate(\DateTime $start_date) {
+    $this->start_date = clone($start_date);
+  }
+
+  /**
+   * @return \DateTime
+   */
+  public function getStartDate() {
+    return $this->start_date;
+  }
+
+  /**
+   * @param \DateTime $end_date
+   */
+  public function setEndDate(\DateTime $end_date) {
+    $this->end_date = $end_date;
+  }
+
+  /**
+   * @param \DateTime $end_date
+   * @return \DateTime
+   */
+  public function getEndDate(\DateTime $end_date) {
+    return $this->end_date;
+  }
+
+  /**
+   * @param \Roomify\Bat\Unit\Unit $unit
+   */
+  public function setUnit(Unit $unit) {
+    $this->unit = $unit;
+  }
+
+  /**
+   * @return \Roomify\Bat\Unit\Unit
+   */
+  public function getUnit() {
+    return $this->unit;
+  }
+
+  /**
+   * @param $events
+   * @return mixed
+   */
+  public abstract function determineValue($events);
+
 }
