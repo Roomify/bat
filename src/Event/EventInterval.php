3c1b7aff8f2f29299f2fb911358c9a0427fd0d75
Nicolò Caruso <nicolo@istos.it>
2016-03-15 10:56:34 +0100

    Don't use timestamp due to a php date bug

--- src/Event/EventInterval.php
+++ src/Event/EventInterval.php
@@ -16,27 +16,30 @@ namespace Roomify\Bat\Event;
  */
 class EventInterval {
 
-	/**
-	 * Return the number of times a duration fits into the start and end date taking into account
-	 * BAT's consideration that the end time for a BAT event includes that last minute.
-	 *
-	 * @param \DateTime $start_date
-	 * @param \DateTime $end_date
-	 * @param \DateInterval $duration
-	 *
-	 * @return float
-	 */
-	public static function divide(\DateTime $start_date, \DateTime $end_date, \DateInterval $duration) {
-		// Clone so that we don't change the original object. We are not using ImmutableDateTime to support PHP5.4
-		$temp_end_date = clone($end_date);
+  /**
+   * Return the number of times a duration fits into the start and end date taking into account
+   * BAT's consideration that the end time for a BAT event includes that last minute.
+   *
+   * @param \DateTime $start_date
+   * @param \DateTime $end_date
+   * @param \DateInterval $duration
+   *
+   * @return float
+   */
+  public static function divide(\DateTime $start_date, \DateTime $end_date, \DateInterval $duration) {
+    // Clone so that we don't change the original object. We are not using ImmutableDateTime to support PHP5.4
+    $temp_end_date = clone($end_date);
 
-		// Add a minute to deal with the fact that BAT considers the last minute included
-		$temp_end_date->add(new \DateInterval('PT1M'));
+    // Add a minute to deal with the fact that BAT considers the last minute included
+    $temp_end_date->add(new \DateInterval('PT1M'));
 
-		// Convert everything to seconds and calculate exactly how many times the duration fits in our event length
-		$duration_seconds = $duration->d * 86400 + $duration->h * 3600 + $duration->i * 60 + $duration->s;
+    // Convert everything to seconds and calculate exactly how many times the duration fits in our event length
+    $duration_seconds = $duration->d * 86400 + $duration->h * 3600 + $duration->i * 60 + $duration->s;
 
-		return ($temp_end_date->getTimestamp() - $start_date->getTimestamp()) / $duration_seconds;
-	}
+    $diff = $start_date->diff($temp_end_date);
+    $diff_seconds = $diff->d * 86400 + $diff->h * 3600 + $diff->i * 60 + $diff->s;
+
+    return $diff_seconds / $duration_seconds;
+  }
 
 }
